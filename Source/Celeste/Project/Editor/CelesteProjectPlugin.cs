5d275c4b6bd3ec3d9b15b4355e9d07f00fe37f72
Alan Wills <alawills@googlemail.com>
2023-10-04 19:28:57 +0100

    Adding some extra stuff to ignore and updating the game editor target with the build folder

---
+++ Source/Celeste/Project/Editor/CelesteProjectPlugin.cs
@@ -0,0 +1,37 @@
+﻿using FlaxEditor;
+using FlaxEditor.GUI;
+using FlaxEditor.GUI.ContextMenu;
+using FlaxEngine;
+
+namespace CelesteEditor.Project
+{
+    public class CelesteProjectPlugin : EditorPlugin
+    {
+        private ContextMenuButton _button;
+
+        /// <inheritdoc />
+        public override void InitializeEditor()
+        {
+            base.InitializeEditor();
+
+            _button = Editor.UI.AddMenuButton("Celeste/Code Generation", "Add Module", OnMenuItemPressed);
+        }
+
+        /// <inheritdoc />
+        public override void DeinitializeEditor()
+        {
+            if (_button != null)
+            {
+                _button.Dispose();
+                _button = null;
+            }
+
+            base.DeinitializeEditor();
+        }
+
+        private void OnMenuItemPressed()
+        {
+            MessageBox.Show("Button clicked!");
+        }
+    }
+}
